a17d4f70e74ef1629c8b72f31bde55abb269ded0
khacctin <108194687+khacctin1746@users.noreply.github.com>
2026-03-14 14:46:31 +0700

    update

---
+++ Bai2_2/Bai2_2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+namespace Bai2_2
+{
+    public class DSPhanSo
+    {
+        private double[] tu;
+        private double[] mau;
+        
+        //constructor
+        public DSPhanSo()
+        {
+            tu = new double[2];
+            mau = new double[2];
+        }
+        
+        public void Nhap()
+        {
+            for (int i = 0; i < tu.Length; i++)
+            {
+                Console.Write("Vui long nhap tu so cua phan so thu " + i + ":" );
+                tu[i] = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Vui long nhap mau so cua phan so thu " + i + ":" );
+                mau[i] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+
+        public void Xuat()
+        {
+            for (int i = 0; i < tu.Length; i++)
+            {
+                Console.WriteLine(tu[i] + "/"  + mau[i]);
+            }
+        }
+
+        public double Chia(double tu, double mau)
+        {
+            double n = tu/mau;
+            return n;
+        }
+
+        public void timPhanSoLonNhat()
+        {
+            double n = 0;
+            double maxTu = 0;
+            double maxMau = 0;
+            
+            for (int i = 0; i < tu.Length; i++)
+            {
+                if (Chia(tu[i], mau[i]) > n)
+                {
+                    n = Chia(tu[i], mau[i]);
+                    maxTu = tu[i];
+                    maxMau = mau[i];
+                }
+            }
+            
+            Console.WriteLine(maxTu + "/" + maxMau);
+        }
+
+        public void SapXep()
+        {
+            for (int i = 0; i < tu.Length; i++)
+            {
+                for (int j = i; j < tu.Length; j++)
+                {
+                    if (Chia(tu[j], mau[j]) < Chia(tu[i], mau[i]))
+                    {
+                        double sav1;
+                        double sav2;
+                        sav1 = tu[i];
+                        sav2 = mau[i];
+                        tu[i] = tu[j];
+                        mau[i] = mau[j];
+                        tu[j] = sav1;
+                        mau[j] = sav2;
+                    }
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DSPhanSo ds = new DSPhanSo();
+            ds.Nhap();
+            ds.Xuat();
+            ds.timPhanSoLonNhat();
+            ds.SapXep();
+        }
+    }
+}
--- PhanSo/PhanSo/PhanSo.cs
+++ PhanSo/PhanSo/PhanSo.cs
@@ -7,7 +7,74 @@ namespace PhanSo
         private double MauSo;
         private double TuSo1;
         private double MauSo2;
+        
+        //constructor default
+        public PhanSo()
+        {
+            TuSo = 1;
+            MauSo = 1;
+            TuSo1 = 1;
+            MauSo2 = 1;
+        }
+        
+        //constructor
+        public PhanSo(double tuSo, double mauSo, double tuSo1, double mauSo2)
+        {
+            this.TuSo = tuSo;
+            this.setMauSo(mauSo);
+            this.TuSo1 = tuSo1;
+            this.setMauSo1(mauSo2);
+        }
+        //getter
+        public double getTuSo()
+        {
+            return TuSo;
+        }
 
+        public double getMauSo()
+        {
+            return MauSo;
+        }
+
+        public double getTuSo1()
+        {
+            return TuSo1;
+        }
+
+        public double getMauSo1()
+        {
+            return MauSo2;
+        } 
+        
+        //setter
+        public void setTuSo(double tuSo)
+        {
+            this.TuSo = tuSo;
+        }
+
+        public void setMauSo(double MauSo)
+        {
+            if (!(MauSo == 0))
+            {
+                this.MauSo = MauSo;
+            }
+            else
+            {
+                Console.WriteLine("Mau so khong duoc be hon khong");
+            }
+        }
+
+        public void setMauSo1(double MauSo2)
+        {
+            if (!(MauSo2 == 0))
+            {
+                this.MauSo2 = MauSo2;
+            }
+            else
+            {
+                Console.WriteLine("Mau so khong duoc be hon khong");
+            }
+        }
         public void Nhap()
         {
             TuSo = Convert.ToDouble(Console.ReadLine());
