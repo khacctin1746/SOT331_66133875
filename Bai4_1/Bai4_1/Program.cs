dcdfa20b747e7883588f5a75db818b8f5602ce6f
khacctin <108194687+khacctin1746@users.noreply.github.com>
2026-03-28 07:36:10 +0700

    update

---
+++ Bai4_1/Bai4_1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Bai4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            Console.WriteLine("Vui long nhap n: "); 
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i * i <= n; i++)
+            {
+                while (n % i == 0)
+                {
+                    stack.Push(i);
+                    n /= i;
+                }
+            }
+
+            if (n > 1)
+            {
+                stack.Push(n);
+            }
+            
+            // stack.Peek();
+            // stack.isEmpty();
+            stack.PrintStack();
+        }
+    }
+}
---
+++ Bai4_1/Bai4_1/Stack.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+
+namespace Bai4_1;
+
+public class Stack
+{
+    private int top;
+    private int max;
+    ArrayList stack;
+
+    public Stack()
+    {
+        top = -1;
+        max = 0;
+        stack = new ArrayList();
+    }
+    
+    public Stack(int top, int max)
+    {
+        this.top = top;
+        this.max = max;
+        stack = new ArrayList();
+    }
+
+    public void Push(int data)
+    {
+        stack.Add(data);
+    }
+
+    public void Pop()
+    {
+        stack.RemoveAt(stack.Count - 1);
+    }
+
+    public void Peek()
+    {
+        top = (int)stack[stack.Count - 1];
+        Console.WriteLine(top);
+    }
+    
+    public void isEmpty()
+    {
+        if (stack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine("Stack isn't empty");
+        }
+    }
+
+    public void PrintStack()
+    {
+        for (int i = 0; i < stack.Count; i++)
+        {
+            Console.WriteLine(stack[i]);
+        }
+    }
+}
